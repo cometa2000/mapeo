b7fc388e5dcc538009dac06e7c272d1141f0bf6a
cometa2000 <tuxmorro2016@gmail.com>
2022-11-26 18:11:58 -0600

    version 1

---
+++ mapeo_circulo/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body {
+            background-color: gray;
+        }
+
+        .circulo path{
+            stroke: #5D11E3;
+            stroke-width: 2px;
+        }
+        .circulo :hover{
+            fill: cadetblue;
+            cursor: pointer;
+            stroke: #5D11E3;
+        }
+
+    </style>
+
+</head>
+
+<body>
